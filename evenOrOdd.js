ddf21c87a23df8ef9280d5e6061cb555709e8506
Arun Joseph <24806953+arunjoseph001@users.noreply.github.com>
2023-08-20 08:44:10 +0100

    Use ternary operator for solution

--- evenOrOdd.js
+++ evenOrOdd.js
@@ -2,7 +2,8 @@
 //for even numbers or "Odd" for odd numbers.
 
 function evenOrOdd(number) {
-  if(number%2===0){
-    return 'Even'
-  } return 'Odd'
+  // if(number%2===0){
+  //   return 'Even'
+  // } return 'Odd'
+  return number % 2 ===0 ? 'Even' : 'Odd'
 }
