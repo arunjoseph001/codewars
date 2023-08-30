bbc55b519a9a3549a263e5f53d4dd5ac76cf51c4
Arun Joseph <24806953+arunjoseph001@users.noreply.github.com>
2023-08-30 12:33:49 +0100

    Complete bonus kata

---
+++ lvl8/grasshopper.js
@@ -0,0 +1,13 @@
+// Write a program that finds the summation of every number from 1 to num. 
+//The number will always be a positive integer greater than 0.
+
+let summation = function (num) {
+  let total=0
+  for(i=1;i<=num;i++){
+    total+=i
+  }
+}
+
+// const summation = n => n * (n + 1) / 2;
+
+summation(8)
